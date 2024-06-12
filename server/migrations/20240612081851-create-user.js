ff36e19dd103947fa86d30f79dd9bd1a8088ad07
Borys Ushakov <inlvert@gmail.com>
2024-06-12 12:21:22 +0300

    add user model + maigrate

---
+++ server/migrations/20240612081851-create-user.js
@@ -0,0 +1,60 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        field: 'first_name'
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull:false,
+        field: 'last_name'
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      isMale: {
+        type: Sequelize.BOOLEAN,
+        field: 'is_male'
+      },
+      birthday: {
+        type: Sequelize.DATEONLY
+      },
+      bonusAmount: {
+        type: Sequelize.DECIMAL,
+        field: 'bonus_amount',
+        defaultValue: 10
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at'
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
---
+++ server/models/user.js
@@ -0,0 +1,58 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: "first_name",
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        field: 'last_name'
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING
+      },
+      role: {
+        type: DataTypes.STRING
+      },
+      isMale: {
+        type: DataTypes.BOOLEAN,
+        field: 'is_male'
+      },
+      birthday: {
+        type: DataTypes.DATEONLY
+      },
+      bonusAmount: {
+        type: DataTypes.DECIMAL,
+        field: 'bonus_amount',
+        defaultValue: 10
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: 'users',
+      underscored: true
+    }
+  );
+  return User;
+};
