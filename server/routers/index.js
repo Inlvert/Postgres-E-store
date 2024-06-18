0baf6148d5857ea48de0784f51c46a2b8183cac0
Borys Ushakov <inlvert@gmail.com>
2024-06-18 15:59:42 +0300

    add connect routers

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,7 +1,15 @@
+const cartRouter = require('./cartRouter');
+const productRouter = require('./productRouter');
+const productToCartRouter = require('./productToCartRouter');
 const userRouter = require('./userRouter');
 
 const router = require('express').Router();
 
 router.use('/users', userRouter)
+router.use('/carts', cartRouter)
+router.use('/products', productRouter)
+
+router.use('/increment', productToCartRouter)
+
 
 module.exports = router;
