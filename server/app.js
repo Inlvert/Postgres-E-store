9afe04934d11bc4d1684c8537b2b335f46dfec7e
Borys Ushakov <inlvert@gmail.com>
2024-06-21 10:40:19 +0300

    added cors on server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = require("./routers");
 const basicErrorHendler = require("./middlewares/errors/basic");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 app.use(basicErrorHendler);
 
