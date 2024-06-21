fa2cf780f89039bb7c95aec09b088b78daa37719
Borys Ushakov <inlvert@gmail.com>
2024-06-21 10:37:04 +0300

    create pages

---
+++ client/src/pages/Cart/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const CartPage = () => {
+  return (
+    <div>
+      <div>
+        <h1>CartPage</h1>
+      </div>
+      <Header />
+    </div>
+  );
+};
+
+export default CartPage;
---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/Header";
+import ProductForm from "../../components/ProductForm";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <h1>HomePage</h1>
+      </div>
+      <Header />
+      <ProductForm/>
+    </div>
+  );
+};
+
+export default HomePage;
