e0df3230ad6a14a68234410719e124d7d025a92d
Borys Ushakov <inlvert@gmail.com>
2024-06-24 13:56:24 +0300

    add connect pruduct list to app

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import ProductForm from "../../components/ProductForm";
+import ProductList from "../../components/ProductList";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       </div>
       <Header />
       <ProductForm/>
+      <ProductList/>
     </div>
   );
 };
