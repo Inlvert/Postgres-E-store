e39ad30d7cc1fc5a5d6eaa6765f3e6c55435b8ef
Borys Ushakov <inlvert@gmail.com>
2024-06-24 13:53:35 +0300

    add thunk and redusers in Slice

--- client/src/redux/slice/productSlice.js
+++ client/src/redux/slice/productSlice.js
@@ -15,12 +15,31 @@ const createProduct = createAsyncThunk(
     try {
       const response = await API.createProduct(productData);
 
-      const { data: { data: product } } = response;
+      const {
+        data: { data: product },
+      } = response;
 
       return product;
     } catch (error) {
       console.log(error);
 
+      return thunkAPI.rejectWithValue(error.response.data.data.errors);
+    }
+  }
+);
+
+const getProducts = createAsyncThunk(
+  `${SLICE_NAME}/get`,
+  async (productData, thunkAPI) => {
+    try {
+      const response = await API.getProducts(productData);
+
+      const {
+        data: { data: products },
+      } = response;
+
+      return products;
+    } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.data.errors);
     }
   }
@@ -41,11 +60,22 @@ const productSlice = createSlice({
       state.isLoading = true;
       state.error = action.payload;
     });
+    builder.addCase(getProducts.pending, (state) => {
+      state.isLoading = false;
+    });
+    builder.addCase(getProducts.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.product = action.payload;
+    });
+    builder.addCase(getProducts.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
   },
 });
 
 const { reducer: productsReducer, actions } = productSlice;
 
-export { createProduct };
+export { createProduct, getProducts };
 
 export default productsReducer;
