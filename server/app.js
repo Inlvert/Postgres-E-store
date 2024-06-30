59089bb595898b56e06afe1e02912b19ef14c607
Borys Ushakov <inlvert@gmail.com>
2024-06-30 18:18:08 +0300

    add mw static

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const router = require("./routers");
 const basicErrorHendler = require("./middlewares/errors/basic");
 const cors = require("cors");
 
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
 app.use(basicErrorHendler);
+
+app.use(express.static('public'))
 
 module.exports = app;
