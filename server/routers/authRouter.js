bc512c7e34d78d4f5f30f299fd966a88834ff199
Borys Ushakov <inlvert@gmail.com>
2024-07-10 14:51:07 +0300

    add cheack Refresh Token on router

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -1,9 +1,10 @@
 const authRouter = require('express').Router();
 const authControllre = require('../controllers/auth.controller');
+const { cheackRefreshToken } = require('../middlewares/token.mw');
 const { imageUpload } = require('../utils/imageUpload');
 
 authRouter.post('/registartion', imageUpload.single("avatar"), authControllre.registration)
 authRouter.post('/login', authControllre.login)
-authRouter.post('/refresh', authControllre.refresh)
+authRouter.post('/refresh', cheackRefreshToken, authControllre.refresh)
 
 module.exports = authRouter;
