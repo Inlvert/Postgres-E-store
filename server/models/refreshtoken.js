fe9c9466da58429da184706530cd91eadde6339c
Borys Ushakov <inlvert@gmail.com>
2024-07-10 14:52:13 +0300

    add refresh function in auth controller

--- server/models/refreshtoken.js
+++ server/models/refreshtoken.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       RefreshToken.belongsTo(models.User, {
-        foreignKey: 'userId'
-      })
+        foreignKey: "userId",
+      });
     }
   }
   RefreshToken.init(
