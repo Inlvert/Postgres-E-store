21cd11160da255507dddd3c54507695a557499e2
Borys Ushakov <inlvert@gmail.com>
2024-07-10 14:34:04 +0300

    add token mw + connect to app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = require("./routers");
 const basicErrorHendler = require("./middlewares/errors/basic");
+const tokenErrorHendler = require("./middlewares/errors/token");
 const cors = require("cors");
 
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
+app.use(tokenErrorHendler);
 app.use(basicErrorHendler);
 
 app.use(express.static('public'))
