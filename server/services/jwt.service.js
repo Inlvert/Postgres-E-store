5298d4a033cf61bc66bc479c0766ead756083147
Borys Ushakov <inlvert@gmail.com>
2024-07-11 16:01:43 +0300

    add connect constants to jwt service

--- server/services/jwt.service.js
+++ server/services/jwt.service.js
@@ -1,30 +1,34 @@
 const jwt = require("jsonwebtoken");
 const { promisify } = require("util");
+const CONSTANTS = require("../constants");
 
 const jwtSign = promisify(jwt.sign);
 const jwtVerify = promisify(jwt.verify);
 
 const tokenConfig = {
   access: {
-    secret: "sfbdfuiodoklsdv84546dvfv",
-    expiresIn: "1min" 
+    secret: CONSTANTS.ACCES_TOKEN_SECRET,
+    expiresIn: CONSTANTS.ACCES_TOKEN_EXPIRES_IN,
   },
   refresh: {
-    secret: "dbkjhklljshdkjsjdlkfjd",
-    expiresIn: "5d" 
-  }
-}
+    secret: CONSTANTS.REFRESH_TOKEN_SECRET,
+    expiresIn: CONSTANTS.REFRESH_TOKEN_EXPIRES_IN,
+  },
+};
 
-const createToken = (payload, { secret, expiresIn }) => jwtSign(payload, secret, {expiresIn});
+const createToken = (payload, { secret, expiresIn }) =>
+  jwtSign(payload, secret, { expiresIn });
 
-const verifyToken = (token, {secret}) => jwtVerify(token, secret);
+const verifyToken = (token, { secret }) => jwtVerify(token, secret);
 
 module.exports.createTokenPair = async (payload) => {
   return {
     accessToken: await createToken(payload, tokenConfig.access),
-    refreshToken: await createToken(payload, tokenConfig.refresh)
-  }
-}
+    refreshToken: await createToken(payload, tokenConfig.refresh),
+  };
+};
 
-module.exports.verifyAccessToken = token => verifyToken(token, tokenConfig.access);
-module.exports.verifyRefreshToken = token => verifyToken(token, tokenConfig.refresh);
+module.exports.verifyAccessToken = (token) =>
+  verifyToken(token, tokenConfig.access);
+module.exports.verifyRefreshToken = (token) =>
+  verifyToken(token, tokenConfig.refresh);
