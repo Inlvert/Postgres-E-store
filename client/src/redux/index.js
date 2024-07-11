19614abf4395546c1726aae3fc8ee81e42d61a6d
Borys Ushakov <inlvert@gmail.com>
2024-07-11 22:56:06 +0300

    add auth slice

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./slice/productSlice";
 import userReducer from "./slice/userSlice";
+import authReducer from "./slice/authSlice";
 
 const store = configureStore({
   reducer: {
     product: productsReducer,
-    user: userReducer
+    user: userReducer,
+    auth: authReducer,
   },
 });
 
---
+++ client/src/redux/slice/authSlice.js
@@ -0,0 +1,51 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import * as API from "../../api";
+
+const SLICE_NAME = "auth";
+
+const login = createAsyncThunk(
+  `${SLICE_NAME}/login`,
+  async (userData, thunkAPI) => {
+    try {
+      const {
+        data: {
+          data: { user },
+        },
+      } = await API.login(userData);
+
+      return user;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.data.errors);
+    }
+  }
+);
+
+const initialState = {
+  user: null,
+  isLoading: false,
+  error: null,
+};
+
+const authSlise = createSlice({
+  name: SLICE_NAME,
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(login.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.isLoading = true;
+      state.user = action.payload;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      state.isLoading = true;
+      state.error = action.payload;
+    });
+  },
+});
+
+const { reducer: authReducer, actions } = authSlise;
+
+export { login };
+
+export default authReducer;
