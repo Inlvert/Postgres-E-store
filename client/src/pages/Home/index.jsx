3ae5dd99b7f7415d519b41af7879dd1882a4b75c
Borys Ushakov <inlvert@gmail.com>
2024-07-16 15:44:38 +0300

    add clear token

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -2,20 +2,29 @@ import React from "react";
 import Header from "../../components/Header";
 import ProductForm from "../../components/ProductForm";
 import ProductList from "../../components/ProductList";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/slice/authSlice";
+import { clearTokens } from "../../api";
 
 const HomePage = () => {
-  const {user} = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const hendleLogout = () => {
+    dispatch(logout());
+    clearTokens();
+  };
+
+  const { user } = useSelector((state) => state.auth);
   return (
     <div>
       <div>
         <h1>HomePage</h1>
-        <h1>Hello {user ? `${user.firstName} ${user.lastName}` : 'guest'}</h1>
+        <h1>Hello {user ? `${user.firstName} ${user.lastName}` : "guest"}</h1>
+        <button onClick={hendleLogout}>logout</button>
       </div>
       <Header />
-      <ProductForm/>
-      <ProductList/>
-
+      <ProductForm />
+      <ProductList />
     </div>
   );
 };
